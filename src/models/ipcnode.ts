cd3b28d8f4dca3443d26cef6aedb14361f97d70d
gbenm <benyamin.galeano@gmail.com>
2022-02-01 00:25:43 -0600

    refactor: Now the IPCNode instance must be built

--- src/models/ipcnode.ts
+++ src/models/ipcnode.ts
@@ -1,1 +1,39 @@
-export class IPCNode {}
+// Esta clase debe manejar el envío de datos
+// en forma Raw, es decir no aplicar ninguna transformación
+class IPCBaseNode {}
+
+// Esta clase permite agregar transformaciones de
+// datos, es decir que se apoya de las funciones
+// de IPCBaseNode para enviar los datos, pero
+// aplica transformaciones antes.
+class IPCInnerNode extends IPCBaseNode implements IPCNode {
+  name!: string
+
+  subscribe(channel: string) {
+    return channel
+  }
+}
+
+export interface IPCNode {
+  get name(): string
+
+  subscribe(channel: string): unknown
+}
+
+export class IPCNodeBuilder {
+  private _ipc: IPCInnerNode = new IPCInnerNode()
+
+  public setName(name: string): IPCNodeBuilder {
+    this._ipc.name = name
+    return this
+  }
+
+  public reset(): IPCNodeBuilder {
+    this._ipc = new IPCInnerNode()
+    return this
+  }
+
+  public build(): IPCNode {
+    return this._ipc
+  }
+}
